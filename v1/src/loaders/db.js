db369027099771556910437d94c5b81f068a4e76
ymzEmre <dev.yilmazemre@gmail.com>
2022-03-15 14:58:06 +0300

    Deploy to heroku

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -7,7 +7,7 @@ db.once('open', () => {
 });
 
 const connectDB = async () => {
-  await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+  await Mongoose.connect('mongodb+srv://ymzemre:ymzEmre!m77@birthday-reminder.i5kod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
